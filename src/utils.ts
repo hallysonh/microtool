754e2cf9e0368c303566b2dfc122147001d6909d
Hallyson Almeida <hallysonh@gmail.com>
2018-02-19 13:06:04 -0300

    fix(build): Fixed definition to String.trimLine function

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,9 @@
-/// <reference path="./string.d.ts" />
-
 const CONSECUTIVE_SPACES = /^\x20+|\x20+$/gm;
 const NEWLINES_MATCH = /[\r\n]+/g;
+
+declare interface String {
+  trimLine() : string;
+}
 
 String.prototype.trimLine = function(this: string) {
   return this.replace(CONSECUTIVE_SPACES, "").replace(NEWLINES_MATCH, " ").trim();
